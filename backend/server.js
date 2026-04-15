723695b2e929fc9e82e05b68d4e198fca5988ee2
Somiran Dutta <somirandutta@Somirans-MacBook-Air.local>
2026-04-15 21:37:12 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
-// backend/index.js or backend/server.js (main server file)
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const router = require('./router/UserRoutes.js');
 const authroutes = require('./router/UserAuthRoutes.js');
 const adminRoutes = require('./router/adminRoutes.js');
-const feedbackRoutes = require('./router/feedbackRoutes.js'); // Add this line
-const paymentRoutes = require('./router/paymentRoutes.js'); // Add this line
+const feedbackRoutes = require('./router/feedbackRoutes.js'); 
+const paymentRoutes = require('./router/paymentRoutes.js'); 
 const connect = require('./db/connection.js');
 const dotenv = require('dotenv');
 const authMiddleware = require('./middleware/authMiddleware.js');
@@ -16,8 +15,6 @@ dotenv.config();
 const app = express();
 
 
-/* middleware */
-// app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -28,35 +25,26 @@ app.get('/', (req, res) => {
     res.status(201).json("Home get request")
 });
 
-/* api routes */
-/** USER Auth Routes */
 app.use('/api/auth', authroutes);
-
 app.use('/api/payment',authMiddleware,paymentRoutes);
-
-/** FEEDBACK Routes */
 app.use('/api/feedback', feedbackRoutes);
-
-/** ADMIN Routes */
 app.use('/api/admin', (req, res, next) => {
     if (req.path === '/login') {
-        next(); // Skip authMiddleware for this route
+        next(); 
     } else {
         authMiddleware(req, res, next);
     }
 }, adminRoutes);
 
-/** USER Routes */
 app.use('/api', (req, res, next) => {
     if (req.path === '/searchFlight' || req.path==='/searchAllFlights') {
-        next(); // Skip authMiddleware for this route
+        next(); 
     } else {
         authMiddleware(req, res, next);
     }
 }, router);
 
 
-/* start server only when we have valid connection */
 connect(process.env.CONNECTION_STRING).then(() => {
     try {
         app.listen(port, () => {
